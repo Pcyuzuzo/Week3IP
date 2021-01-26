a0dd1487a76a9ecd1caca47b2e19385ce524c6ab
Pcyuzuzo <PCyuzuzo320@daviscollege.com>
2021-01-26 17:31:14 +0200

    added

---
+++ script.js
@@ -0,0 +1,55 @@
+$(function () {
+    let design = $('#design');
+    let development = $('#development');
+    let product = $('#product');
+    let destext = $('#destxt');
+    let devtext = $('#devtext');
+    let protext = $('#protext');
+    $(design).hide();
+    $(development).hide();
+    $(product).hide();
+    $('.des').on('click', function () {
+        $('.des').slideUp('slow');
+        $(design).show(500);
+        $(destext).animate({fontWeight:'bolder',fontSize:'30px'})
+    });
+    $('.dev').on('click', function () {
+        $('.dev').slideUp('slow');
+        $(development).show(500);
+        $(devtext).animate({fontWeight:'bolder',fontSize:'30px'})
+    });
+    $('.pro').on('click', function () {
+        $('.pro').slideUp('slow');
+        $(product).show(500);
+        $(protext).animate({fontWeight:'bolder',fontSize:'30px'})
+    });
+    $(design, destext).on('click', function () {
+        $('.des').slideDown();
+        $(design).hide(500);
+        $(destext).animate({fontWeight:'bolder',fontSize:'16px'})
+    });
+    $(development, devtext).on('click', function () {
+        $('.dev').slideDown();
+        $(development).hide(500);
+        $(devtext).animate({fontWeight:'normal',fontSize:'16px'})
+    });
+    $(product, protext).on('click', function () {
+        $('.pro').slideDown();
+        $(product).hide(500);
+        $(protext).animate({fontWeight:'normal',fontSize:'16px'})
+    })
+    $('.port').hover(function(){
+        $('.porttext',this).slideToggle('milliseconds');
+     }, function(){
+        $('.porttext',this).slideToggle('milliseconds');
+     });
+        $("form#form").on('submit',function(event){
+            event.preventDefault();
+            let name = $("input#name").val();
+            let email = $("input#email").val();
+            let message = $("textarea#message").val();
+            if ($("input#name").val() && $("input#email").val()){
+                alert ("Hey " + name + "," +"Thanks for your comment, we'll comeback to you soon");
+            }
+        });
+});
